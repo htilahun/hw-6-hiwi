d4d9e6cfd6283a5e42713a15a81d242a90a3913f
htilahun <Hiwi2@Hiwots-MacBook-Air.local>
2016-07-31 19:17:48 -0400

    lesson-06-hw-hiwot
    homework for lesson 06 - Hiwot

--- hw-lesson06-hiwot/js/java.js
+++ hw-lesson06-hiwot/js/java.js
@@ -8,13 +8,13 @@ function cityBackground() {
 	} else if (input ==="la" || input === "los angeles") {
 		$("body").css("background-image", "url(images/la.jpg)")	
 	} else if (input==="austin") {
-		$("body").css("background-image", "url(images/austin.jpg")
+		$("body").css("background-image", "url(images/austin.jpg)")
 	
 	}else if (input==="sf" || input === "san francisco") {
-		$("body").css("background-image", "url(images/sf.jpg")
+		$("body").css("background-image", "url(images/sf.jpg)")
 	
 	}else if (input==="sydney") {
-		$("body").css("background-image", "url(images/sydney.jpg")
+		$("body").css("background-image", "url(images/sydney.jpg)")
 
 	}else
 		$("body").css("background-image", "url(images/citipix_skyline.jpg)")
