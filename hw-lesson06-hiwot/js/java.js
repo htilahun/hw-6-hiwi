ba4e3539dcc1d8fa3c41e66e326171d7f5adb1ca
htilahun <Hiwi2@Hiwots-MacBook-Air.local>
2016-08-01 17:54:42 -0400

    Minor addition
    ;

--- hw-lesson06-hiwot/js/java.js
+++ hw-lesson06-hiwot/js/java.js
@@ -4,20 +4,20 @@ function cityBackground() {
 	var input = $("input[type='text']").val().toLowerCase();
 
 	if (input === "nyc" || input === "new york") {
-		$("body").css("background-image", "url(images/nyc.jpg)")
+		$("body").css("background-image", "url(images/nyc.jpg)");
 	} else if (input ==="la" || input === "los angeles") {
-		$("body").css("background-image", "url(images/la.jpg)")	
+		$("body").css("background-image", "url(images/la.jpg)");	
 	} else if (input==="austin") {
-		$("body").css("background-image", "url(images/austin.jpg)")
+		$("body").css("background-image", "url(images/austin.jpg)");
 	
 	}else if (input==="sf" || input === "san francisco") {
-		$("body").css("background-image", "url(images/sf.jpg)")
+		$("body").css("background-image", "url(images/sf.jpg)");
 	
 	}else if (input==="sydney") {
-		$("body").css("background-image", "url(images/sydney.jpg)")
+		$("body").css("background-image", "url(images/sydney.jpg)");
 
 	}else
-		$("body").css("background-image", "url(images/citipix_skyline.jpg)")
+		$("body").css("background-image", "url(images/citipix_skyline.jpg)");
 	}
 	
 
